0f70f40ba5c4833b9c1ed57dd88b1199bc0bb5df
merlins-code <github.6arzv@slmails.com>
2025-06-12 15:07:08 -0500

    Add files via upload

---
+++ even_or_odd_project.2.py
@@ -0,0 +1,20 @@
+# start with a number
+
+try:
+    number = int(input("Tell me a number and I will tell you if it's even or odd:\n"))
+except ZeroDivisionError:
+    print("You can't devide by zero!")
+except ValueError:
+    print("Thats not a number. Try again.")
+except Exception:
+    print("Something went wrong.")
+finally:
+    
+
+
+# Find out if its even or odd
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("odd")
