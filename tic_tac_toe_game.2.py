e34ac5e9c71c4ef969fa7746fc551adb804405c9
merlins-code <github.6arzv@slmails.com>
2025-06-17 19:21:45 -0500

    Update tic_tac_toe_game.2.py

--- tic_tac_toe_game.2.py
+++ tic_tac_toe_game.2.py
@@ -1,13 +1,30 @@
-# First step
 
+mylist =[]
 
-my_list = []
-num_items = int(input("Enter the number of items:\n "))
-for i in range(num_items):
-    item = input(f"Enter item {i+1}:\n ")
-    my_list.append(item)
-print(my_list)
-  
+# Ask for how manny objects are in the list
+
+number = int(input("how manny Items do you have"))
+
+#the for loop creates something that repeats
+# This stors the for operation in I
+# Then it sets the rang of objects that are in the aray
+# This range comes from the input above
+
+for i in range(number):
+    
+    #this allows the user to input the ogject at that space
+    # then i+1 puts the number in the current i and then moves forward one spot
+
+    item = input(f"Enter item{i+1}:\n")
+    
+    # this takes my item and adds it to the end of my list
+    
+    mylist.append(item)
+    
+    # this prints the current list once the loop is done
+    
+print(mylist)
+      
   
 
 
