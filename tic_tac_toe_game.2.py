5bf5a58e71d3d61c688b6a3ce30109f71d9e8630
merlins-code <github.6arzv@slmails.com>
2025-06-25 22:32:59 -0500

    Update tic_tac_toe_game.2.py

--- tic_tac_toe_game.2.py
+++ tic_tac_toe_game.2.py
@@ -1,8 +1,45 @@
+# # Create a variable to store the board and set it to an empty list.
+
 board = []
-player = "X"
-    
-for i in range(3):
-    for j in range(3):
-        board.append([i,j])
-print(board)
-   
+
+# #Create the variable to store the size of the board and set it to 3.
+
+size = 3
+
+# # Loop Through the range of the size of the board
+
+for row in range(size):
+
+# # Append an empty list to the board for each row.
+
+    board.append([])
+
+# # loop through the rang of the size of the board.
+
+    for col in range(size):
+
+# # Append an empty string to each row for each column.
+
+        
+        
+
+
+# # Loop through the outer list of the board.
+
+
+
+# # loop through the inner list of each row.
+
+
+
+# # Print the value of wach cell in the row seperated with a pipe
+
+
+                  
+
+
+
+# # Print  horizontal line after each row
+
+
+
