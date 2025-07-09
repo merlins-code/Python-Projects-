28523a4163036d1df085a9a6760514a1594c2aa1
merlins-code <github.6arzv@slmails.com>
2025-07-08 19:27:58 -0500

    Update tic_tac_toe_game.2.py

--- tic_tac_toe_game.2.py
+++ tic_tac_toe_game.2.py
@@ -1,36 +1,32 @@
-size = 4
-player = "X"
+
+board = []
+size = 3
 rungame = True
 
-board = [["" for _ in range(size)] for _ in range(size)]
-
 def printboard(board):
-    for i in range(size):
-        row_display = ""
-        for j in range(size):
-            cell = board[i][j] if board[i][j] != "" else " "
-            row_display += f" {cell} "
-            if j < size - 1:
-                row_display += "|"
-        print(row_display)
-        if i < size - 1:
-            print("-" * (size * 3 + (size - 1)))
-
-def playerinput(board):   
-    try:
-        inrow = int(input(f"what row do you pick? (1-{size})\n"))
-        incol = int(input(f"What column do you pick? (1-{size})\n"))
-        if 1 <= inrow <= size and 1 <= incol <= size:
-            if board[inrow - 1][incol - 1] == "":
-                board[inrow - 1][incol - 1] = player
-            else:
-                print("That spot is already taken.")
-        else:
-            print("That's outside of the board size")
-    except ValueError:
-        print("That's not a number so try again.")
-
-
-while rungame:
-    printboard(board)
-    playerinput(board)
+  for row in range(size):
+    board.append([])
+    for col in range(size):        
+        board[row].append("")
+        
+  for i in range(len(board)):
+    for j in range(len(board[i])):
+      if j < size - 1:
+          print(" | ", end="")
+    print()
+    if i < size - 1:
+      print("-" * (size * 3 - 3))
+      
+def playerinput(board):
+        try:
+          inrow = int(input("what row do you pick?\n"))
+          incol = int(input("What collumn do you pick?\n"))
+          if incol <= size and inrow <= size:
+            print(f"you picked\n{inrow}:{incol}")
+          else:
+            print("Thats outside of the board size")
+        except ValueError:
+            print("Thats not a number so try again.")
+            
+printboard(board)
+playerinput(board)
