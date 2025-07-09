c170c12b146131fb725a4b4d8412535c08bbed9a
merlins-code <github.6arzv@slmails.com>
2025-07-08 21:23:03 -0500

    Update tic_tac_toe_game.2.py

--- tic_tac_toe_game.2.py
+++ tic_tac_toe_game.2.py
@@ -1,32 +1,35 @@
 
-board = []
+
 size = 3
 rungame = True
+player = "X"
+board = [["" for i in range(size)] for i in range(size)]
 
-def printboard(board):
-  for row in range(size):
-    board.append([])
-    for col in range(size):        
-        board[row].append("")
-        
+def printboard(board):  
   for i in range(len(board)):
     for j in range(len(board[i])):
+      print(board[i][j], end="")
       if j < size - 1:
-          print(" | ", end="")
+        print(" | ", end="")
     print()
     if i < size - 1:
-      print("-" * (size * 3 - 3))
+        print("-" * (size * 3 - 3))
       
 def playerinput(board):
         try:
           inrow = int(input("what row do you pick?\n"))
           incol = int(input("What collumn do you pick?\n"))
           if incol <= size and inrow <= size:
-            print(f"you picked\n{inrow}:{incol}")
+            if board[inrow - 1][incol - 1] == "":
+                board[inrow - 1][incol - 1] = player
           else:
             print("Thats outside of the board size")
         except ValueError:
             print("Thats not a number so try again.")
+        
             
-printboard(board)
-playerinput(board)
+
+while rungame:
+  printboard(board)
+  playerinput(board)
+  
